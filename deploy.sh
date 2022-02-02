c756d1533c61aebb70c2fbfc74d6609dbaeca432
Patrisha deBoon <PLdeBoon@pcl.com>
2022-02-02 10:53:22 -0700

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@https://github.com/Patrisha-de-Boon/Patrisha-de-Boon.github.io.git master:gh-pages
+git push -f git@github.com:Patrisha-de-Boon/Patrisha-de-Boon.github.io.git master:gh-pages
 cd -
